9d720dbc5192cea65d0e00f644216f5f9a8e9a29
Shortyd77 <shortydee34@gmail.com>
2026-01-12 12:41:52 -0800

    Correct variable names and improve element access
    Fix variable names and update element retrieval method.

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,6 @@
 // Create varibables for the welcome message
-var greeting = ' Hello ';
-var name = ' Welcome to Sunshine Photography';
+var greeting = 'Hello ';
+var name = 'Welcome to Sunshine Photography';
 var message = ', How can I help you today:';
 //Concatenate the three variables above to create a welcome message
 var welcome = greeting + name + message;
@@ -8,9 +8,6 @@ var welcome = greeting + name + message;
 // Combine the message
 var welcomeMessage = greeting + name + message;
 
-// Get the greeting element and update it
-var elGreeting = document.getElementbyId ("greeting")
-elGreeting.textContent = welcomemessage;
-
-
+// Get the element that has an id of greeting
+var el = document.getELementById('greeting');
 
