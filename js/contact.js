6b730ad4573b00bc42e6254f6d8f3fe84e237c86
Shortyd77 <shortydee34@gmail.com>
2026-01-12 12:14:48 -0800

    Correct welcome message assignment and element retrieval
    Fix variable name and method call for greeting message.

--- js/contact.js
+++ js/contact.js
@@ -5,5 +5,12 @@ var message = ', How can I help you today:';
 //Concatenate the three variables above to create a welcome message
 var welcome = greeting + name + message;
 
+// Combine the message
+var welcomeMessage = greeting + name + message;
+
+// Get the greeting element and update it
+var elGreeting = document.getElementbyId ("greeting")
+elGreeting.textContent = welcomemessage;
+
 
 
