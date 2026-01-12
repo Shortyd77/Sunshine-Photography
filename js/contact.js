059b4a8e3329a338bc5419e3335d574ef9f1c62a
Shortyd77 <shortydee34@gmail.com>
2026-01-12 11:49:06 -0800

    Update greeting and name variables for welcome message

---
+++ js/contact.js
@@ -0,0 +1,9 @@
+// Create varibables for the welcome message
+var greeting = ' Hello ';
+var name = ' Welcome to Sunshine Photography';
+var message = ', How can I help you today:';
+//Concatenate the three variables above to create a welcome message
+var welcome = greeting + name + message;
+
+
+
