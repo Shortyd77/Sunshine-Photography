1f3f6bd0c85b6a3c5e3a1e89a5f6431618f672de
Shortyd77 <shortydee34@gmail.com>
2026-01-13 07:59:20 -0800

    Remove extra space in welcome message

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,8 @@
 // Create variables for the welcome message
 var greeting = 'Hello, ';
 var name = 'welcome to Sunshine Photography!';
-var message = ' Capturing memories one click at a time. ';
+var message = ' Capturing memories one click at a time.';
+
 
 // Concatenate the three variables above to create the welcome message
 var welcome = greeting + name + message;
