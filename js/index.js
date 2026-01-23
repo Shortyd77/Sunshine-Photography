66a61075c37fcbc85ec51684e51f242cb5eeaffb
Shortyd77 <shortydee34@gmail.com>
2026-01-23 09:41:15 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,7 +31,48 @@
     }
 
     today = new Date(); // Get today's date
+    
     elEnds = document.getElementById('offerEnds'); // Find the HTML element
     elEnds.innerHTML = offerExpires(today); // Add the message to the page
+
+
+
+(function() {
+    var photos = [5,10,15,20]; // x # of photographs
+    var pricePerPhoto = 15;   //price per photo
+    var tableHTML = '<table><tr><th>Photos</th><th>Price</th></tr>
+
+// The Loop: Generates the rows of the table
+    for (var i =0; i < photos.length; i++) {
+        var rowTotal = photos[i] * pricePerPhoto;
+        tableHTML += '<tr><td>' + photos[i] + 'Digital Images</td><td>$ + rowTotal + '</td></tr>';
+    }
+    tableHTML += '</table>';
+    document.getElementById(;pricingTable').innerHTML = tableHTML;
+
+    //THE DECISION: Apply a filter/discount message if x number is selected
+     var selectedForQuote =20;
+        bonusMsg = "";
+
+    if (selectedForQuote >=20) {
+        bonusMsg = "<strong>Elite Choice:</strong> Booking 20+ photos unlocks a custom 'Golden Hour' filter for your gallery!";
+    } else { 
+        bonusMsg = "Book 20 photos to unlock custom color grading.";
+    }
+    document.getElementById('bulkBonus').innerHTML = bonusMsg;
+}());
+
+
+
+
+
+
+
+
+
+
+
+
+    
 
 }());
