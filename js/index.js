d1c6a5337e3cdb42fb94b257b1a265c9dc002947
Shortyd77 <shortydee34@gmail.com>
2026-01-23 09:57:13 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
-/* the script is placed inside an immediately invoked function expression which helps protect the scope of variables */
-
-/* the script is placed inside an immediately invoked function expression which helps protect the scope of variables */
 (function() {
-    // PART ONE: OFFER EXPIRY LOGIC
+    // PART ONE: 25% OFFER EXPIRY
     var expiryMsg; 
     var today;
     var elEnds;
@@ -29,14 +26,15 @@
         elEnds.innerHTML = offerExpires(today);
     }
 
-    // PART TWO: PRICING TABLE (LOOP & DECISION)
+    // PART TWO: PRICING LOOP & DECISION
     var photos = [5, 10, 15, 20]; 
     var pricePerPhoto = 15;   
     var tableHTML = '<table><tr><th>Photos</th><th>Price</th></tr>';
 
-    // The Loop: Generates the rows of the table
+    // The Loop: Building the table rows
     for (var i = 0; i < photos.length; i++) {
         var rowTotal = photos[i] * pricePerPhoto;
+        // Fixed the quote marks in the line below
         tableHTML += '<tr><td>' + photos[i] + ' Digital Images</td><td>$' + rowTotal + '</td></tr>';
     }
     tableHTML += '</table>';
@@ -46,12 +44,12 @@
         pricingTableEl.innerHTML = tableHTML;
     }
 
-    // THE DECISION: Apply a message if a certain number is met
+    // The Decision: Bonus message logic
     var selectedForQuote = 20;
     var bonusMsg = "";
 
     if (selectedForQuote >= 20) {
-        bonusMsg = "<strong>Elite Choice:</strong> Booking 20+ photos unlocks a custom 'Golden Hour' filter for your gallery!";
+        bonusMsg = "<strong>Elite Choice:</strong> Booking 20+ photos unlocks a custom filter!";
     } else { 
         bonusMsg = "Book 20 photos to unlock custom color grading.";
     }
@@ -60,20 +58,6 @@
     if (bulkBonusEl) {
         bulkBonusEl.innerHTML = bonusMsg;
     }
-
 }());
-
-
-
-
-
-
-
-
-
-
-
-
     
 
-}());
