81a14f568d29c0579261182f2e61d68ffdb1ad35
Shortyd77 <shortydee34@gmail.com>
2026-02-13 10:11:35 -0800

    Correct typos in map.js for Google Maps setup
    Fix typos in function and variable names for Google Maps initialization.

--- js/map.js
+++ js/map.js
@@ -1,1 +1,17 @@
+fucntion init() {
+  var mapOptions = {
+    center: new google.maps.LatLng(48.2931, -122.6436), // Coordinates for Oak Harbor, Wa
+    mapTypeld: google.maps.MapTypeid.ROADMAP,
+    Zoom:11
+  };
+var venueMap;
+  venueMap = new google.maps.Map(document.getElementByld('map'), mapOptions);
+}
 
+function loadScript() {
+  var script=document.createElement('script');
+  script.src ="http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+    document.body.appendChild(script);
+}
+
+window.onload = loadScript;
