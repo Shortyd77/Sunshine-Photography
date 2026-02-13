a963295926fd82efda0b4b8a508f3e722513e153
Shortyd77 <shortydee34@gmail.com>
2026-02-13 10:28:23 -0800

    Fix typos in map.js for map initialization

--- js/map.js
+++ js/map.js
@@ -1,17 +1,17 @@
 function init() {
   var mapOptions = {
     center: new google.maps.LatLng(48.2931, -122.6436), // Coordinates for Oak Harbor, Wa
-    mapTypeld: google.maps.MapTypeid.ROADMAP,
-    Zoom:11
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom:11
   };
 
   var venueMap;
-  venueMap = new google.maps.Map(document.getElementByld('map'), mapOptions);
+  venueMap = new google.maps.Map(document.getElementById('map'), mapOptions);
 }
 
 function loadScript() {
   var script = document.createElement('script');
-  script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=init';
+  script.src = "http://maps.googleapis.com/maps/api/js?sensor=false&callback=init";
     document.body.appendChild(script);
 }
 
