3aab3d0dc856a91776b38ac864e0d51145d52d90
Danielle Thompson <shortydee34@gmail.com>
2026-02-25 14:36:31 -0800

    Add gallery filter and sort functionality

---
+++ js/gallery-filter-sort.js
@@ -0,0 +1,73 @@
+$(function() {
+    var $gallery = $('#gallery-container');
+    var $cards = $('.gallery');
+    var $search = $('#photoSearch');
+    var $buttons = $('#filter-buttons');
+    var tagged = {};
+    var cache = [];
+
+    // 1. PREPARE DATA (Search Cache & Filter Tags)
+    $cards.each(function() {
+        var card = this;
+        var tags = $(this).data('tags');
+        var altText = $(this).find('img').attr('alt').toLowerCase();
+
+        // Add to search cache
+        cache.push({
+            element: card,
+            text: altText
+        });
+
+        // Add to tags object
+        if (tags) {
+            tags.split(',').forEach(function(tagName) {
+                tagName = tagName.trim();
+                if (tagged[tagName] == null) { tagged[tagName] = []; }
+                tagged[tagName].push(card);
+            });
+        }
+    });
+
+    // 2. CREATE FILTER BUTTONS
+    $('<button/>', {
+        text: 'Show All',
+        class: 'active',
+        click: function() {
+            $(this).addClass('active').siblings().removeClass('active');
+            $cards.fadeIn();
+        }
+    }).appendTo($buttons);
+
+    $.each(tagged, function(tagName) {
+        $('<button/>', {
+            text: tagName,
+            click: function() {
+                $(this).addClass('active').siblings().removeClass('active');
+                $cards.hide().filter(tagged[tagName]).fadeIn();
+            }
+        }).appendTo($buttons);
+    });
+
+    // 3. SEARCH FUNCTIONALITY
+    function filterSearch() {
+        var query = this.value.trim().toLowerCase();
+        cache.forEach(function(obj) {
+            var index = obj.text.indexOf(query);
+            $(obj.element).toggle(index !== -1);
+        });
+    }
+    $search.on('input', filterSearch);
+
+    // 4. SORTING FUNCTIONALITY (By Tag Name)
+    $('#sort-az').on('click', function() {
+        $cards.sort(function(a, b) {
+            return $(a).data('tags') > $(b).data('tags') ? 1 : -1;
+        }).appendTo($gallery);
+    });
+
+    $('#sort-za').on('click', function() {
+        $cards.sort(function(a, b) {
+            return $(a).data('tags') < $(b).data('tags') ? 1 : -1;
+        }).appendTo($gallery);
+    });
+});
