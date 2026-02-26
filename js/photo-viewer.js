02a351e711af1aa4207d0a5485dbac1756657930
Danielle Thompson <shortydee34@gmail.com>
2026-02-26 13:42:30 -0800

    Update photo-viewer.js

--- js/photo-viewer.js
+++ js/photo-viewer.js
@@ -1,14 +1,62 @@
+var request;
+var $current;
+var cache = {};
+var $frame = $('#photo-viewer');
+var $thumbs = $('.thumb');
+
 function crossfade($img) {
   if ($current) {
     $current.stop().fadeOut('slow');
   }
 
   // Let CSS handle centering
-  $img.css({
-    marginLeft: 0,
-    marginTop: 0
-  });
+  $img.css({ marginLeft: 0, marginTop: 0 });
 
   $img.stop().fadeTo('slow', 1);
   $current = $img;
 }
+
+$(document).on('click', '.thumb', function (e) {
+  var $img,
+    src = this.href;
+
+  request = src;
+  e.preventDefault();
+
+  $thumbs.removeClass('active');
+  $(this).addClass('active');
+
+  if (cache.hasOwnProperty(src)) {
+    if (cache[src].isLoading === false) {
+      crossfade(cache[src].$img);
+    }
+  } else {
+    $img = $('<img/>');
+    cache[src] = {
+      $img: $img,
+      isLoading: true
+    };
+
+    $img.on('load', function () {
+      // Keep measurable but invisible at first
+      $img.css({ opacity: 0, display: 'block' });
+
+      $frame.removeClass('is-loading').empty().append($img);
+      cache[src].isLoading = false;
+
+      if (request === src) {
+        crossfade($img);
+      }
+    });
+
+    $frame.addClass('is-loading');
+
+    $img.attr({
+      src: src,
+      alt: this.title || ''
+    });
+  }
+});
+
+// Load the first thumbnail automatically
+$('.thumb').eq(0).click();
